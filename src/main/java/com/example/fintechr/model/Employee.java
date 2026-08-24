218fa54d0a875e1679e1b2cabddaa3b43dad9bf2
NicolasIsepe <nicolasisepe@gmail.com>
2026-08-24 07:48:26 -0300

    feat: add employee model and status enum

---
+++ src/main/java/com/example/fintechr/model/Employee.java
@@ -0,0 +1,24 @@
+package com.example.fintechr.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+import com.example.fintechr.model.enums.Status;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private String role;
+    private String department;
+    private BigDecimal salary;
+    private String city;
+    private Status status;
+}
