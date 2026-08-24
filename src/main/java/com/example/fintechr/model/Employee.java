fa8ba1aef2ee85c739425cdb98e6fd89ae083106
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-24 08:55:41 -0300

    test: add unit test for get all employees

--- src/main/java/com/example/fintechr/model/Employee.java
+++ src/main/java/com/example/fintechr/model/Employee.java
@@ -1,6 +1,7 @@
 package com.example.fintechr.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -9,6 +10,7 @@ import java.math.BigDecimal;
 import com.example.fintechr.model.enums.Status;
 
 @Data
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Employee {
---
+++ src/test/java/com/example/fintechr/repository/EmployeeServiceTest.java
@@ -0,0 +1,56 @@
+package com.example.fintechr.repository;
+
+import com.example.fintechr.model.Employee;
+import com.example.fintechr.model.enums.Status;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import com.example.fintechr.repository.EmployeeRepository;
+import com.example.fintechr.service.EmployeeService;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+    @Mock
+    private EmployeeRepository repository;
+    @InjectMocks
+    private EmployeeService service;
+
+    @Test
+    void shouldReturnAllEmployees() {
+        var employees = List.of(
+                createEmployee(1L, "John Doe", "john.email@email.com"),
+                createEmployee(2L, "Bob", "bob.ishere@email.com"),
+                createEmployee(3L, "Alice", "alice.inwonder@email.com")
+        );
+
+        when(repository.findAll()).thenReturn(employees);
+
+        var result = service.getAll();
+
+        assertEquals(employees, result);
+        verify(repository).findAll();
+    }
+
+    private Employee createEmployee(Long id, String name, String email) {
+        return Employee.builder()
+                .id(id)
+                .name(name)
+                .email(email)
+                .phone("11999999999")
+                .role("Software Engineer")
+                .department("Technology")
+                .salary(new BigDecimal("7500.00"))
+                .city("São Paulo")
+                .status(Status.HIRED)
+                .build();
+    }
+}
