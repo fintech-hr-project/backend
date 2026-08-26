b8fc10cdd7b6d5a192df96c0857cb057adcd5c0f
NicolasIsepe <nicolasisepe@gmail.com>
2026-08-26 12:29:59 -0300

    fix: add content import

--- src/test/java/com/example/fintechr/controller/EmployeeControllerTest.java
+++ src/test/java/com/example/fintechr/controller/EmployeeControllerTest.java
@@ -17,6 +17,7 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,7 +50,7 @@ public class EmployeeControllerTest {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-        var employeeJson = objectMapper.writeValueAsString(employee)
+        var employeeJson = objectMapper.writeValueAsString(employee);
 
         mockMvc.perform(post("/employees")
                 .contentType("application/json")
