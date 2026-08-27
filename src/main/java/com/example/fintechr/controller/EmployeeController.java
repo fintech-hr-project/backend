ac75a9109d5168808859dd525b28f71ab4e0385e
Vini-Boas <vinicius.vboas1@gmail.com>
2026-08-26 21:54:07 -0300

    fix: fix getEmployeeById method in controller

--- src/main/java/com/example/fintechr/controller/EmployeeController.java
+++ src/main/java/com/example/fintechr/controller/EmployeeController.java
@@ -24,7 +24,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Employee> getAllEmployees(@PathVariable Long id) {
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
         return ResponseEntity.ok(employeeService.findEmployeeById(id));
     }
 
